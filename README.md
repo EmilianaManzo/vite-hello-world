647cb77f138f69f4abaf9ee9019e2dd47d9fa95f
EmilianaManzo <emiliana.ma96@gmail.com>
2024-03-20 15:08:04 +0100

    [add] hello world

--- README.md
+++ README.md
@@ -5,3 +5,20 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Vue - Official](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (previously Volar) and disable Vetur
+
+
+# Title : Vite Hello World
+
+Descrizione:
+Create un nuovo progetto utilizzando Vite: aiutatevi con le slide per ripercorrere i vari passaggi dell’installazione come visti a lezione.
+Create nei data un titolo e un paragrafo da stampare nel template.
+Create degli stili globali e degli stili specifici del componente App.vue
+
+### Svolgimento 
+1. creo una cartella dal power shell 
+2. do il nome alla cartella che sarà anche il nome della repo 
+3. scelgo il framework Vue 
+4. scelgo la variante
+5. apro il progetto da power shell 
+6. nel terminal invio i comandi install e poi run dev
+7.
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import bootstrap from 'bootstrap';
+
+export default {
+  data(){
+    return{
+      title: 'Hello World' ,
+      paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, facere iste tempore deserunt iusto distinctio corporis eum quibusdam numquam ullam, quaerat eligendi voluptatum quasi aliquam fugit possimus, tempora laudantium assumenda.'
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class=" d-flex justify-content-center align-items-center text-center ">
+    <h1 class=" text-center">{{ title }}</h1>
+    <span><i class="fa-solid fa-earth-europe"></i></span>
+    <p>{{ paragraph }} </p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  
 </style>
